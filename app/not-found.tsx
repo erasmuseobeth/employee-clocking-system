46adad87e6387e63983caf60080a3cd75c676766
Codeprince001 <urangyorkwisdom@gmail.com>
2025-03-21 01:23:15 +0100

    fix: next Link error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
@@ -5,12 +7,12 @@ export default function NotFound() {
       <h2 className="text-2xl font-semibold text-gray-800 mt-2">Page Not Found</h2>
       <p className="text-gray-600 mt-2">Oops! The page you are looking for does not exist.</p>
 
-      <a
+      <Link
         href="/"
         className="mt-6 px-6 py-2 bg-primary text-white rounded-lg hover:bg-primary transition"
       >
         Go Back Home
-      </a>
+      </Link>
 
       <img
         src="https://illustrations.popsy.co/white/resistance-band.svg"
