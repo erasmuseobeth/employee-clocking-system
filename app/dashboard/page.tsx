47cb38a89de7bba02bad58298a8bfa5a31918acb
erasmuseobeth <cyber330d@gmail.com>
2025-03-21 06:51:55 +0100

    fixed Deployement issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
     return (
-      <div>
+      <div className="pt-32">
         <h1>Dashboard</h1>
         <p>Welcome to your dashboard.</p>
       </div>
